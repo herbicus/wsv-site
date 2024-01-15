5ed3c9a9b00173c2d333ba4d112e6fac2ddeeff9
Herb Torres <herbtorres960@gmail.comm>
2024-01-14 22:34:50 -0500

    adds some meta icons

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { NextSeo } from 'next-seo';
-import React from 'react';
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
+import React from "react";
 
 type MetaProps = {
   title?: string;
@@ -19,12 +19,9 @@ const Meta: React.FC<MetaProps> = (props) => {
         <meta charSet="UTF-8" key="charset" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <title>Grandma&#8217;s House - Ann Arbor</title>
+        <title>Whiskey Shit Vomit - Band Site</title>
 
-        <meta
-          name="description"
-          content="Make yourself at home in Ann Arbor's welcoming west side neighborhood."
-        />
+        <meta name="description" content="Whiskey Shit Vomit - Band Site" />
 
         <meta
           name="viewport"
@@ -32,8 +29,8 @@ const Meta: React.FC<MetaProps> = (props) => {
           key="viewport"
         />
         <link
-          rel="apple-touch-icon"
-          href={`${router.basePath}/apple-touch-icon.png`}
+          rel="apple-icon"
+          href={`${router.basePath}/apple-icon.png`}
           key="apple"
         />
         <link
@@ -61,16 +58,15 @@ const Meta: React.FC<MetaProps> = (props) => {
         description={props.description}
         canonical={props.canonical}
         openGraph={{
-          title: 'Grandma\u2019s House - Ann Arbor',
-          description:
-            'Make yourself at home in Ann Arbor\u2019s welcoming west side neighborhood.',
-          url: 'www.graandmashouse.com',
-          locale: 'en',
-          site_name: 'Grandma\u2019s House - Ann Arbor',
+          title: "Whiskey Shite Vomit - Band Site",
+          description: "Whiskey Shit Vomit - Band Site",
+          url: "www.wsv.herbtorres.com",
+          locale: "en",
+          site_name: "Whiskey Shite Vomit - Band Site",
           images: [
             {
               url: `${router.basePath}/meta-op-image.jpg`,
-              alt: 'Grandma\u2019s House - Ann Arbor',
+              alt: "Whiskey Shite Vomit - Band Site",
             },
           ],
         }}
